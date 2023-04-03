db3406c4e7bb3b890317ea62b718393ddbc3367e
wojteknowacki <wojciech.nowacki@saleor.io>
2023-04-03 12:57:05 +0200

    wip

--- cypress/e2e/demo/demo.cy.ts
+++ cypress/e2e/demo/demo.cy.ts
@@ -1,11 +1,13 @@
 describe("Search for products", () => {
-  beforeEach(() => {
-    cy.visit("https://demo.saleor.io/channel-pln/pl-PL/");
-  });
+  beforeEach(() => {});
 
   it("should search for products SRS_0405", () => {
-    cy.get('[data-testid="categoriesListAll records"]').should("be.visible");
-    cy.get('[data-testid="category"]').should("be.visible");
-    cy.get('[data-testid="footerExternalLinksGraphQL API"]').should("be.visible");
+    cy.addAliasToGraphRequest("ProductCollection");
+    cy.visit("https://demo.saleor.io/channel-pln/pl-PL/")
+      .wait("@ProductCollection")
+      .wait(10000)
+      .then(() => {
+        cy.percySnapshot("Storefront demo - main page");
+      });
   });
 });
--- cypress/elements/main-page.ts
+++ cypress/elements/main-page.ts
@@ -1,4 +1,6 @@
 export const MAIN_PAGE = {
   categorySection: "[data-testid='category']",
   categoryName: "[data-testid*='categoryName']",
+  productList: "[data-testid='productsList']",
+  img: "img",
 };
